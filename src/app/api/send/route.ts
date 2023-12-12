6079707b48c7f463e3cc05c75cec1cb8a65d6613
KyrychenkoDmytro <107756841+KyrychenkoDmytro@users.noreply.github.com>
2023-12-12 19:53:17 +0200

    d

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -7,18 +7,20 @@ import * as React from 'react';
 export async function POST(request: Request) {
   const resend = new Resend(process.env.RESEND_API_KEY);
   const body = await request.json();
-  const { fullName, telegram, email, message } = body;
+  const { fullName, telegram, email, header, message } = body;
 
   try {
     const { data, error } = await resend.emails.send({
-      from: 'Train Courses <onboarding@resend.dev>',
+      from: "Усвідомлене Управління <onboarding@resend.dev>",
       to: 'test.resend12@gmail.com',
-      subject: 'Form Data',
+      subject: "Зворотній зв'язок",
       reply_to: email as string,
       react: React.createElement(EmailFeedbackData, {
         fullName: fullName as string,
         telegram: telegram as string,
-        message: message as string
+        email: email as string,
+        message: message as string,
+        header: header as string
       })
     });
 
